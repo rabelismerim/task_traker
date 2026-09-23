15e98547e3213ffc5bc281c36fe39c32f239a4da
Robert Abel Ismerim <r.abelismerim@gmail.com>
2026-09-23 20:07:03 -0300

    implementacao login jwt

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Task(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tasks')
     title = models.CharField(max_length=200)
     completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
